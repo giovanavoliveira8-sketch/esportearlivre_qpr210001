91345aa320d9a56e068345207632bae19ffac120
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-13 16:43:12 -0300

    Definido as rotas quando o usuário abir o link

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AtletaComponentComponent } from './component/atleta-component/atleta-component.component';
+import { HomeComponentComponent } from './component/home-component/home-component.component';
+
+export const routes: Routes = [
+
+    {
+        path: "",
+        redirectTo: "/home",
+        pathMatch: "full"
+    },
+
+    {
+        path: "home",
+        component: HomeComponentComponent
+    },
+
+    {
+        path: "cadastroatleta",
+        component: AtletaComponentComponent
+    }
+
+    
+];
