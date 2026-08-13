ea11e9ee49d6bea83ebb20535dacf54045439e78
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-13 16:42:10 -0300

    Retirado as importaçôes que não seriam mais utilizadas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,10 @@ import { RouterOutlet } from '@angular/router';
 
 import { MenuComponentComponent } from './component/menu-component/menu-component.component';
 
-import { AtletaComponentComponent } from './component/atleta-component/atleta-component.component';
-
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MenuComponentComponent, AtletaComponentComponent],
+  imports: [RouterOutlet, MenuComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
