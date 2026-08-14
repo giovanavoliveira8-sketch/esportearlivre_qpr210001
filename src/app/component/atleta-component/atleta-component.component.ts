9ab8c26bb39aed409c07a881a6e1a4e53962f1dc
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-14 14:23:30 -0300

    Declarado os atributos e as funções

--- src/app/component/atleta-component/atleta-component.component.ts
+++ src/app/component/atleta-component/atleta-component.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-atleta-component',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.component.html',
   styleUrl: './atleta-component.component.css'
 })
 export class AtletaComponentComponent {
+  //DECLARANDO ATRIBUTOS
+  nome = ''
+  cpf = 0
+  sexo = ''
+  cep =  0
+  ruaLogradouro = ''
+  bairro = ''
+  cidade = ''
+  uf = ''
 
+  //DECLARAÇÃO DAS FUNÇÕES
+  exibirDados(){
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.ruaLogradouro, this.bairro, this.cidade, this.uf)
+  }
 }
