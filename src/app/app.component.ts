8e75e760ecc534b59a837e3c6d8e982ca55e005d
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-14 13:40:08 -0300

    Importado o RouterLink

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
 import { MenuComponentComponent } from './component/menu-component/menu-component.component';
 
 @Component({
