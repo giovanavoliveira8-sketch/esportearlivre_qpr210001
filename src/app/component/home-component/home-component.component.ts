538cd7d4ed7f310ab100ece050a517a492fcd1e6
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-14 13:38:34 -0300

    Importado o RouterLink

--- src/app/component/home-component/home-component.component.ts
+++ src/app/component/home-component/home-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
