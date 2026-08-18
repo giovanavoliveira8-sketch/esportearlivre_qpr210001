ecf5372eee0038af78a2b18b0ed4ff0879d8a5fd
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-18 14:02:18 -0300

    Comentado e

--- src/app/service/atleta-service.service.ts
+++ src/app/service/atleta-service.service.ts
@@ -1,12 +1,43 @@
 import { Injectable } from '@angular/core';
-import { Atleta } from '../component/models/Atleta';
+import { Atleta } from '../models/Atleta';
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AtletaServiceService {
-       //DECLARANDO ARRAY atletas
+
+    constructor(private http: HttpClient) { }
+
+    listarAtletas(): Observable<Atleta[]> {
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
+        return this.http.get<Atleta[]>(urlApi)
+    }
+
+    listarAtleta(idAtleta: number): Observable<Atleta> {
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
+        return this.http.get<Atleta>(urlApi)
+    }
+
+    salvarAtleta(atleta: Atleta): Observable<Atleta> {
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
+        return this.http.post<Atleta>(urlApi, atleta)
+    }
+
+    excluirAtleta(idAtleta: number): Observable<Atleta> {
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
+        return this.http.delete<Atleta>(urlApi)
+    }
+
+    alterarAtleta(atleta: Atleta): Observable<Atleta> {
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${atleta.id}`
+
+        return this.http.put<Atleta>(urlApi, atleta)
+    }
+}
+/*DECLARANDO ARRAY atletas
   private atletas: Atleta[] = []
 
       //DECLARANDO DAS FUNÇÕES DE MANIPULAÇÃO DO ARRAY
@@ -39,7 +70,7 @@ export class AtletaServiceService {
   alterarElemento(atleta: Atleta){
     let posArray = this.atletas.findIndex(elem=>elem.id !== atleta.id)
     this.atletas[posArray] = atleta
-  }
-  }
+  } */
+
 
   
