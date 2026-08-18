cc9934d25ffcfffc80d0989cb9ff3b45e528326a
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-18 14:03:18 -0300

    Criado o componente atleta-list

---
+++ src/app/component/atleta-lista/atleta-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-atleta-lista',
+  standalone: true,
+  imports: [],
+  templateUrl: './atleta-lista.component.html',
+  styleUrl: './atleta-lista.component.css'
+})
+export class AtletaListaComponent {
+
+}
