bcf79c30cc1e6d2a9d479d4940aca004cfe390c1
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-18 14:41:02 -0300

    Declarado as funções

--- src/app/component/atleta-component/atleta-component.component.ts
+++ src/app/component/atleta-component/atleta-component.component.ts
@@ -55,7 +55,7 @@ constructor (private atletaService: AtletaServiceService) {}
     atleta.cidade = this.cidade
     atleta.uf = this.uf
 
-    this.atletaService.adicionarAtleta(atleta)
+    this.atletaService.salvarAtleta(atleta)
 
     this.limparDados()
 
