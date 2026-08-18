057924b67fa1bfd4b4878f9d6108362a35241fac
Giovana Vitória <Giovana.v.oliveira8@aluno.senai.br>
2026-08-18 16:50:57 -0300

    Comitado

--- src/app/service/atleta-service.service.ts
+++ src/app/service/atleta-service.service.ts
@@ -12,22 +12,22 @@ export class AtletaServiceService {
     constructor(private http: HttpClient) { }
 
     listarAtletas(): Observable<Atleta[]> {
-        const urlApi = `https://6a84898853754283b0b898a3.mockapi.io/api/v1/atleta`
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
         return this.http.get<Atleta[]>(urlApi)
     }
 
     listarAtleta(idAtleta: number): Observable<Atleta> {
-        const urlApi = `https://6a84898853754283b0b898a3.mockapi.io/api/v1/atleta/${idAtleta}`
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
         return this.http.get<Atleta>(urlApi)
     }
 
     salvarAtleta(atleta: Atleta): Observable<Atleta> {
-        const urlApi = `https://6a84898853754283b0b898a3.mockapi.io/api/v1/atleta`
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta`
         return this.http.post<Atleta>(urlApi, atleta)
     }
 
     excluirAtleta(idAtleta: number): Observable<Atleta> {
-        const urlApi = `https://6a84898853754283b0b898a3.mockapi.io/api/v1/atleta/${idAtleta}`
+        const urlApi = `https://6a7f6d923183f5fd884b1a61.mockapi.io/esportearlivre/atleta/${idAtleta}`
         return this.http.delete<Atleta>(urlApi)
     }
 
